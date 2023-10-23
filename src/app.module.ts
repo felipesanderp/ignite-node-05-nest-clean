495d1ba09c392d30dfd0f75627b492b9638331de
felipesanderp <felipe.panisson@hotmail.com>
2023-10-23 13:36:43 -0300

    jwt auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
+
 import { PrismaService } from './prisma/prisma.service'
-import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
+
 import { AuthenticateController } from './controllers/authenticate.controller'
-import { JwtService } from '@nestjs/jwt'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { CreateQuestionController } from './controllers/create-question.controller'
 
 @Module({
   imports: [
@@ -15,7 +18,11 @@ import { JwtService } from '@nestjs/jwt'
     }),
     AuthModule,
   ],
-  controllers: [CreateAccountController, AuthenticateController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService, JwtService],
 })
 export class AppModule {}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { PassportModule } from '@nestjs/passport'
 import { JwtModule } from '@nestjs/jwt'
 import { ConfigService } from '@nestjs/config'
 import { Env } from 'src/env'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
   imports: [
@@ -11,7 +12,7 @@ import { Env } from 'src/env'
       inject: [ConfigService],
       useFactory(config: ConfigService<Env, true>) {
         const privateKey = config.get('JWT_PRIVATE_KEY', { infer: true })
-        const publicKey = config.get('JWT_PRIVATE_KEY', { infer: true })
+        const publicKey = config.get('JWT_PUBLIC_KEY', { infer: true })
 
         return {
           signOptions: { algorithm: 'RS256' },
@@ -21,5 +22,6 @@ import { Env } from 'src/env'
       },
     }),
   ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
