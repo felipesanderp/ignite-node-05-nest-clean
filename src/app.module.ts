99e439e49841cee1ae0d5fefb7ce9fa58f9eea3c
felipesanderp <felipe.panisson@hotmail.com>
2023-10-23 15:55:49 -0300

    fetch recent questions controller with page param

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { CreateQuestionController } from './controllers/create-question.controll
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
   ],
   providers: [PrismaService, JwtService],
 })
