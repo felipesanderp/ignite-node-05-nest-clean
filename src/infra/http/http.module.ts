a738bb4a1ab7fc0da5b83eae1fac3528834b27a0
felipesanderp <felipe.panisson@hotmail.com>
2023-12-22 13:42:14 -0300

    database module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,16 +4,17 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { PrismaService } from '../prisma/prisma.service'
 import { JwtService } from '@nestjs/jwt'
+import { DatabaseModule } from '../database/database.module'
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [PrismaService, JwtService],
+  providers: [JwtService],
 })
 export class HttpModule {}
