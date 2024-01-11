e902bfe2185adacb5280857fe2d82159b480b4d1
felipesanderp <felipe.panisson@hotmail.com>
2024-01-11 16:11:48 -0300

    prisma repos

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -2,7 +2,7 @@ import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
 export interface AnswerCommentsRepository {
-  create(AnswerComment: AnswerComment): Promise<void>
+  create(answerComment: AnswerComment): Promise<void>
   findById(id: string): Promise<AnswerComment | null>
   findManyByAnswerId(
     answerId: string,
