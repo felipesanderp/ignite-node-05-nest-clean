f32010b6052a2fd179da9e6afadcf91ae2b1647e
felipesanderp <felipe.panisson@hotmail.com>
2024-01-11 16:40:01 -0300

    get question by slug controller and e2e test

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -4,6 +4,7 @@ import { CreateAccountController } from './controllers/create-account.controller
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
 
 import { DatabaseModule } from '../database/database.module'
 
@@ -13,6 +14,7 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
+import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -21,12 +23,14 @@ import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+    GetQuestionBySlugController,
   ],
   providers: [
     CreateQuestionUseCase,
     FetchRecentQuestionsUseCase,
     RegisterStudentUseCase,
     AuthenticateStudentUseCase,
+    GetQuestionBySlugUseCase,
   ],
 })
 export class HttpModule {}
