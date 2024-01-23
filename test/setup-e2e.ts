6b6815eec14232d7a2b1f45d1ff361e7b537e1d8
felipesanderp <felipe.panisson@hotmail.com>
2024-01-23 17:31:20 -0300

    questions repo e2e test to test cache

--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,6 +1,9 @@
 import { config } from 'dotenv'
 import { randomUUID } from 'node:crypto'
 import { execSync } from 'node:child_process'
+import { Redis } from 'ioredis'
+
+import { envSchema } from '@/infra/env/env'
 
 import { DomainEvents } from '@/core/events/domain-events'
 
@@ -9,14 +12,21 @@ import { PrismaClient } from '@prisma/client'
 config({ path: '.env', override: true })
 config({ path: '.env.test', override: true })
 
+const env = envSchema.parse(process.env)
+
 const prisma = new PrismaClient()
+const redis = new Redis({
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  db: env.REDIS_DB,
+})
 
 function generateUniqueDatabaseURL(schemaId: string) {
-  if (!process.env.DATABASE_URL) {
+  if (!env.DATABASE_URL) {
     throw new Error('Please provide a DATABASE_URL environment variable.')
   }
 
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(env.DATABASE_URL)
 
   url.searchParams.set('schema', schemaId)
 
@@ -31,6 +41,8 @@ beforeAll(async () => {
   process.env.DATABASE_URL = databaseURL
 
   DomainEvents.shouldRun = false
+
+  await redis.flushdb()
 
   execSync('pnpm prisma migrate deploy')
 })
